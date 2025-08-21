43939d3a22caf1379ef5f3a7880e15604f2b8817
dasomko <ds92ko@gmail.com>
2025-08-21 15:20:02 +0900

    feat: kana svg에서 path 추출

---
+++ assets/paths/index.ts
@@ -0,0 +1,314 @@
+import { paths as あ } from './03042';
+import { paths as い } from './03044';
+import { paths as う } from './03046';
+import { paths as え } from './03048';
+import { paths as お } from './0304a';
+import { paths as か } from './0304b';
+import { paths as が } from './0304c';
+import { paths as き } from './0304d';
+import { paths as ぎ } from './0304e';
+import { paths as く } from './0304f';
+import { paths as ぐ } from './03050';
+import { paths as け } from './03051';
+import { paths as げ } from './03052';
+import { paths as こ } from './03053';
+import { paths as ご } from './03054';
+import { paths as さ } from './03055';
+import { paths as ざ } from './03056';
+import { paths as し } from './03057';
+import { paths as じ } from './03058';
+import { paths as す } from './03059';
+import { paths as ず } from './0305a';
+import { paths as せ } from './0305b';
+import { paths as ぜ } from './0305c';
+import { paths as そ } from './0305d';
+import { paths as ぞ } from './0305e';
+import { paths as た } from './0305f';
+import { paths as だ } from './03060';
+import { paths as ち } from './03061';
+import { paths as ぢ } from './03062';
+import { paths as っ } from './03063';
+import { paths as つ } from './03064';
+import { paths as づ } from './03065';
+import { paths as て } from './03066';
+import { paths as で } from './03067';
+import { paths as と } from './03068';
+import { paths as ど } from './03069';
+import { paths as な } from './0306a';
+import { paths as に } from './0306b';
+import { paths as ぬ } from './0306c';
+import { paths as ね } from './0306d';
+import { paths as の } from './0306e';
+import { paths as は } from './0306f';
+import { paths as ば } from './03070';
+import { paths as ぱ } from './03071';
+import { paths as ひ } from './03072';
+import { paths as び } from './03073';
+import { paths as ぴ } from './03074';
+import { paths as ふ } from './03075';
+import { paths as ぶ } from './03076';
+import { paths as ぷ } from './03077';
+import { paths as へ } from './03078';
+import { paths as べ } from './03079';
+import { paths as ぺ } from './0307a';
+import { paths as ほ } from './0307b';
+import { paths as ぼ } from './0307c';
+import { paths as ぽ } from './0307d';
+import { paths as ま } from './0307e';
+import { paths as み } from './0307f';
+import { paths as む } from './03080';
+import { paths as め } from './03081';
+import { paths as も } from './03082';
+import { paths as ゃ } from './03083';
+import { paths as や } from './03084';
+import { paths as ゅ } from './03085';
+import { paths as ゆ } from './03086';
+import { paths as ょ } from './03087';
+import { paths as よ } from './03088';
+import { paths as ら } from './03089';
+import { paths as り } from './0308a';
+import { paths as る } from './0308b';
+import { paths as れ } from './0308c';
+import { paths as ろ } from './0308d';
+import { paths as わ } from './0308f';
+import { paths as を } from './03092';
+import { paths as ん } from './03093';
+import { paths as ア } from './030a2';
+import { paths as イ } from './030a4';
+import { paths as ウ } from './030a6';
+import { paths as エ } from './030a8';
+import { paths as オ } from './030aa';
+import { paths as カ } from './030ab';
+import { paths as ガ } from './030ac';
+import { paths as キ } from './030ad';
+import { paths as ギ } from './030ae';
+import { paths as ク } from './030af';
+import { paths as グ } from './030b0';
+import { paths as ケ } from './030b1';
+import { paths as ゲ } from './030b2';
+import { paths as コ } from './030b3';
+import { paths as ゴ } from './030b4';
+import { paths as サ } from './030b5';
+import { paths as ザ } from './030b6';
+import { paths as シ } from './030b7';
+import { paths as ジ } from './030b8';
+import { paths as ス } from './030b9';
+import { paths as ズ } from './030ba';
+import { paths as セ } from './030bb';
+import { paths as ゼ } from './030bc';
+import { paths as ソ } from './030bd';
+import { paths as ゾ } from './030be';
+import { paths as タ } from './030bf';
+import { paths as ダ } from './030c0';
+import { paths as チ } from './030c1';
+import { paths as ヂ } from './030c2';
+import { paths as ッ } from './030c3';
+import { paths as ツ } from './030c4';
+import { paths as ヅ } from './030c5';
+import { paths as テ } from './030c6';
+import { paths as デ } from './030c7';
+import { paths as ト } from './030c8';
+import { paths as ド } from './030c9';
+import { paths as ナ } from './030ca';
+import { paths as ニ } from './030cb';
+import { paths as ヌ } from './030cc';
+import { paths as ネ } from './030cd';
+import { paths as ノ } from './030ce';
+import { paths as ハ } from './030cf';
+import { paths as バ } from './030d0';
+import { paths as パ } from './030d1';
+import { paths as ヒ } from './030d2';
+import { paths as ビ } from './030d3';
+import { paths as ピ } from './030d4';
+import { paths as フ } from './030d5';
+import { paths as ブ } from './030d6';
+import { paths as プ } from './030d7';
+import { paths as ヘ } from './030d8';
+import { paths as ベ } from './030d9';
+import { paths as ペ } from './030da';
+import { paths as ホ } from './030db';
+import { paths as ボ } from './030dc';
+import { paths as ポ } from './030dd';
+import { paths as マ } from './030de';
+import { paths as ミ } from './030df';
+import { paths as ム } from './030e0';
+import { paths as メ } from './030e1';
+import { paths as モ } from './030e2';
+import { paths as ャ } from './030e3';
+import { paths as ヤ } from './030e4';
+import { paths as ュ } from './030e5';
+import { paths as ユ } from './030e6';
+import { paths as ョ } from './030e7';
+import { paths as ヨ } from './030e8';
+import { paths as ラ } from './030e9';
+import { paths as リ } from './030ea';
+import { paths as ル } from './030eb';
+import { paths as レ } from './030ec';
+import { paths as ロ } from './030ed';
+import { paths as ワ } from './030ef';
+import { paths as ヲ } from './030f2';
+import { paths as ン } from './030f3';
+
+interface Paths {
+  d: string;
+  length: number;
+}
+
+interface KanaMap {
+  [key: string]: Paths[];
+}
+
+const kanaMap: KanaMap = {
+  あ,
+  い,
+  う,
+  え,
+  お,
+  か,
+  が,
+  き,
+  ぎ,
+  く,
+  ぐ,
+  け,
+  げ,
+  こ,
+  ご,
+  さ,
+  ざ,
+  し,
+  じ,
+  す,
+  ず,
+  せ,
+  ぜ,
+  そ,
+  ぞ,
+  た,
+  だ,
+  ち,
+  ぢ,
+  っ,
+  つ,
+  づ,
+  て,
+  で,
+  と,
+  ど,
+  な,
+  に,
+  ぬ,
+  ね,
+  の,
+  は,
+  ば,
+  ぱ,
+  ひ,
+  び,
+  ぴ,
+  ふ,
+  ぶ,
+  ぷ,
+  へ,
+  べ,
+  ぺ,
+  ほ,
+  ぼ,
+  ぽ,
+  ま,
+  み,
+  む,
+  め,
+  も,
+  ゃ,
+  や,
+  ゅ,
+  ゆ,
+  ょ,
+  よ,
+  ら,
+  り,
+  る,
+  れ,
+  ろ,
+  わ,
+  を,
+  ん,
+  ア,
+  イ,
+  ウ,
+  エ,
+  オ,
+  カ,
+  ガ,
+  キ,
+  ギ,
+  ク,
+  グ,
+  ケ,
+  ゲ,
+  コ,
+  ゴ,
+  サ,
+  ザ,
+  シ,
+  ジ,
+  ス,
+  ズ,
+  セ,
+  ゼ,
+  ソ,
+  ゾ,
+  タ,
+  ダ,
+  チ,
+  ヂ,
+  ッ,
+  ツ,
+  ヅ,
+  テ,
+  デ,
+  ト,
+  ド,
+  ナ,
+  ニ,
+  ヌ,
+  ネ,
+  ノ,
+  ハ,
+  バ,
+  パ,
+  ヒ,
+  ビ,
+  ピ,
+  フ,
+  ブ,
+  プ,
+  ヘ,
+  ベ,
+  ペ,
+  ホ,
+  ボ,
+  ポ,
+  マ,
+  ミ,
+  ム,
+  メ,
+  モ,
+  ャ,
+  ヤ,
+  ュ,
+  ユ,
+  ョ,
+  ヨ,
+  ラ,
+  リ,
+  ル,
+  レ,
+  ロ,
+  ワ,
+  ヲ,
+  ン
+};
+
+export default kanaMap;
