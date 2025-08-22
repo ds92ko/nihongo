98c0bbc7188ee1db6d3091d43975407474a96420
dasom <ds92ko@gmail.com>
2025-08-23 04:22:18 +0900

    style: font 경로를 절대경로로 변경

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,12 +6,12 @@ import { SafeAreaView, StyleSheet, View } from 'react-native';
 
 export default function RootLayout() {
   const [loaded] = useFonts({
-    'PyeojinGothic-Light': require('../assets/fonts/PyeojinGothic-Light.ttf'),
-    'PyeojinGothic-Regular': require('../assets/fonts/PyeojinGothic-Regular.ttf'),
-    'PyeojinGothic-Medium': require('../assets/fonts/PyeojinGothic-Medium.ttf'),
-    'PyeojinGothic-Bold': require('../assets/fonts/PyeojinGothic-Bold.ttf'),
-    'Cafe24Meongi-B': require('../assets/fonts/Cafe24Meongi-B.ttf'),
-    'Cafe24Meongi-W': require('../assets/fonts/Cafe24Meongi-W.ttf')
+    'PyeojinGothic-Light': require('@/assets/fonts/PyeojinGothic-Light.ttf'),
+    'PyeojinGothic-Regular': require('@/assets/fonts/PyeojinGothic-Regular.ttf'),
+    'PyeojinGothic-Medium': require('@/assets/fonts/PyeojinGothic-Medium.ttf'),
+    'PyeojinGothic-Bold': require('@/assets/fonts/PyeojinGothic-Bold.ttf'),
+    'Cafe24Meongi-B': require('@/assets/fonts/Cafe24Meongi-B.ttf'),
+    'Cafe24Meongi-W': require('@/assets/fonts/Cafe24Meongi-W.ttf')
   });
 
   if (!loaded) return null;
