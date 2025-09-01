977e424485cd59f20eb8cf911752a7dbc214897f
dasomko <ds92ko@gmail.com>
2025-09-01 20:05:35 +0900

    feat: 토스트 작업 중

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,11 @@
+import { Text } from '@/components/common';
 import Dialog from '@/components/common/Dialog';
+import { Colors } from '@/constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
+import { StyleSheet, View } from 'react-native';
+import Toast from 'react-native-toast-message';
 
 export default function RootLayout() {
   const [loaded] = useFonts({
@@ -23,6 +28,107 @@ export default function RootLayout() {
         />
       </Stack>
       <Dialog />
+      <Toast
+        config={{
+          success: ({ text1, text2 }) => (
+            <View style={[styles.toast, styles.success]}>
+              <Ionicons
+                name="checkmark-circle-outline"
+                size={30}
+                color={Colors.success}
+              />
+              <View>
+                <Text
+                  weight={700}
+                  variant="caption"
+                >
+                  {text1}
+                </Text>
+                <Text
+                  variant="caption"
+                  color="textSecondary"
+                >
+                  {text2}
+                </Text>
+              </View>
+            </View>
+          ),
+          error: ({ text1, text2 }) => (
+            <View style={[styles.toast, styles.error]}>
+              <Ionicons
+                name="close-circle-outline"
+                size={30}
+                color={Colors.error}
+              />
+              <View>
+                <Text
+                  weight={700}
+                  variant="caption"
+                >
+                  {text1}
+                </Text>
+                <Text
+                  variant="caption"
+                  color="textSecondary"
+                >
+                  {text2}
+                </Text>
+              </View>
+            </View>
+          ),
+          warning: ({ text1, text2 }) => (
+            <View style={[styles.toast, styles.warning]}>
+              <Text
+                weight={700}
+                variant="caption"
+                color="warning"
+              >
+                {text1}
+              </Text>
+              <Text variant="caption">{text2}</Text>
+            </View>
+          ),
+          info: ({ text1, text2 }) => (
+            <View style={[styles.toast, styles.info]}>
+              <Text
+                weight={700}
+                variant="caption"
+                color="info"
+              >
+                {text1}
+              </Text>
+              <Text variant="caption">{text2}</Text>
+            </View>
+          )
+        }}
+      />
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  toast: {
+    width: '80%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 12,
+    borderRadius: 8,
+    gap: 6
+  },
+  toastContent: {
+    flex: 1,
+    gap: 4
+  },
+  success: {
+    backgroundColor: Colors.successLight
+  },
+  error: {
+    backgroundColor: Colors.errorLight
+  },
+  warning: {
+    backgroundColor: Colors.warningLight
+  },
+  info: {
+    backgroundColor: Colors.infoLight
+  }
+});
